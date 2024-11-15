1838a6de5e51109faaeaaf110858d94e9293815e
AAKKUS1 <aakkus@edenschool.fr>
2024-11-15 15:32:26 +0100

    feat(app)exo6-fix

--- ex06.js
+++ ex06.js
@@ -2,17 +2,17 @@ function getReduction(price, age) {
 	if (age > 25){
       return price
     }
+    else if (age <= 25){
+        return price * 0.7
+      }
+      else if (age <=18){
+        return price - (price * 0.5)
+      }
    else if (age <= 5){
     return 0
   }
-  else if (age <=18){
-    return price - (price * 0.5)
-  }
-  else if (age <= 25){
-    return price - (price * 0.7)
-  }
   
- 
+  
 }
 
 console.log(getReduction(10, 5)); 
